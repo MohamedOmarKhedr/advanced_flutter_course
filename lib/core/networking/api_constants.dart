05e25b232b46b4c80774dda65f626c7249f9a7d4
mohamed omar khedr <mmookk20122000@gmail.com>
2026-02-02 00:01:37 +0200

    sign up feature is done

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants{
   static const apiBaseUrl = 'https://vcare.integration25.com/api/';
   static const login = 'auth/login';
-  static const register = 'auth/register';
+  static const signUp = 'auth/register';
 }
 
 class ApiErrors{
---
+++ lib/featues/sign_up/ui/widgets/sign_up_bloc_listener.dart
@@ -0,0 +1,49 @@
+import 'package:advanced_flutter_course/core/helper/extensions/navigation_extension.dart';
+import 'package:advanced_flutter_course/core/routing/routes.dart';
+import 'package:advanced_flutter_course/core/theming/test_styles.dart';
+import 'package:advanced_flutter_course/featues/sign_up/logic/cubit/sign_up_cubit.dart';
+import 'package:advanced_flutter_course/featues/sign_up/logic/cubit/sign_up_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpBlocListener extends StatelessWidget {
+  final Widget child;
+  const SignUpBlocListener({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SignUpCubit, SignUpState>(
+      listenWhen: (previous, current) => current is Loading || current is Error || current is Success,
+      child: child,
+      listener: (BuildContext context, state) {
+        state.whenOrNull(
+          loading: (){
+
+          },
+          success: (signUpResponse){
+            context.pop();
+            context.pushNamed(Routes.loginScreen);
+          },
+          error: (error){
+            setupErrorState(context, error);
+          }
+        );
+      },
+    );
+  }
+}
+
+void setupErrorState (BuildContext context,String error){
+  context.pop();
+  showDialog(context: context, builder: (context)
+  => AlertDialog(
+    icon: Icon(Icons.error, color: Colors.red,size: 32,
+    ),
+    content: Text(
+      error,style: TextStyles.font15DarkBlueSemiBold,
+    ),
+    actions: [
+      Text('Got it',style: TextStyles.font14DarkBlueMedium,),
+    ],
+  ));
+}
