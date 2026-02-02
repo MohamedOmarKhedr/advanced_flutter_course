e59bd265bc0e4aa976e8394decb884b7e90d6d02
mohamed omar khedr <mmookk20122000@gmail.com>
2026-02-02 09:37:21 +0200

    Add Flovers for Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,25 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    flavorDimensions += "default"
+    productFlavors {
+        create("development") {
+            dimension = "default"
+            resValue(
+                type = "string",
+                name = "app_name",
+                value = "DocDoc Development")
+            applicationIdSuffix = ".dev"
+        }
+        create("production") {
+            dimension = "default"
+            resValue(
+                type = "string",
+                name = "app_name",
+                value = "DocDoc production")
+        }
+    }
 }
 
 flutter {
